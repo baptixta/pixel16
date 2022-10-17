7158186dce3ed0d25cef8deff32c5ab34ebe6ed9
Davi Baptista <daviarcosbaptista@gmail.com>
2022-10-17 20:28:06 -0300

    Feat: enemy template + slime enemy created

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 class_name Player
 
 onready var animation: AnimationPlayer = get_node("AnimationPlayer")
+onready var animationWeapon: AnimationPlayer = get_node("AnimationWeapon")
 onready var sprite: Sprite = get_node("Sprite")
 onready var weapon: Sprite = get_node("Weapon")
 var velocity: Vector2
@@ -13,6 +14,7 @@ func _physics_process(_delta):
 	animate()
 	verifyPosition()	
 	moveWeapon()
+	attack()
 
 func move() -> void:
 	var direction_vector: Vector2 = Vector2(
@@ -35,5 +37,10 @@ func verifyPosition() -> void:
 	elif velocity.x > 0:
 		sprite.flip_h = false
 
-func moveWeapon():
+func moveWeapon() -> void:
 	weapon.look_at(get_global_mouse_position())
+	weapon.rotate(90)
+
+func attack() -> void:
+	if Input.is_action_just_pressed("attack"):
+		animationWeapon.play("weapon_attack")
