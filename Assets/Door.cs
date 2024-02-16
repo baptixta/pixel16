fef8479037d9d83b629ae5a0005e7849e8da8d11
Davi Baptista <daviarcosbaptista@gmail.com>
2024-02-16 18:14:58 -0300

    Feat: Coin + Door prefab

---
+++ Assets/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnterLevel(levelName);
+    }
+
+    public void EnterLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
